3efbfbc5a2bab47214133b840954aeaefada3a47
Shanred <shanred28@gmail.com>
2023-11-12 05:18:38 +0300

    Взаимодействия с НавМешем без агента. Поиск пути до таргета, обновления пути.

--- Assets/Scripts/AI/AIMovement.cs
+++ Assets/Scripts/AI/AIMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public static class TransformExtensions
 {
@@ -26,13 +27,27 @@ public class AIMovement : MonoBehaviour
     [SerializeField] private AIRaySensor _sensorBackward;
     [SerializeField] private AIRaySensor _sensorRight;
     [SerializeField] private AIRaySensor _sensorLeft;
+    [SerializeField] private float _pathUpdateRate;
+    [SerializeField] private float _stopDistance;
 
 
     private Vehicle _vehicle;
     private Vector3 _target;
+    private NavMeshPath _path;
+    private Vector3 _nextPathPoint;
+    private int _cornerIndex;
+
+    private bool _hasPath;
+    private bool _rechedDistination;
+
+    private float _timerUpdatePath;
+
+    public bool HasPatch => _hasPath;
+    public bool RestartDistination => _rechedDistination;
 
     private void Awake()
     {
+        _path = new NavMeshPath();
         _vehicle = GetComponent<Vehicle>();
     }
 
@@ -43,11 +58,81 @@ public class AIMovement : MonoBehaviour
 
     private void Update()
     {
+        if (_pathUpdateRate > 0)
+        { 
+            _timerUpdatePath += Time.deltaTime;
+
+            if (_timerUpdatePath > _pathUpdateRate)
+            {
+                CalculatePath(_target);
+                _timerUpdatePath = 0;
+            }
+
+        }
+
+        UpdateTarget();
+
         MoveToTarget();
+    }
+
+    public void SetDestination(Vector3 target)
+    {
+        if (_target == target && _hasPath == true) return;
+
+        _target = target;
+        CalculatePath(target);
+    }
+
+    public void ResetPath()
+    { 
+        _hasPath = false;
+        _rechedDistination = false;
+    }
+
+    private void UpdateTarget()
+    { 
+        if(_hasPath == false) return;
+
+        _nextPathPoint = _path.corners[_cornerIndex];
+
+        if (Vector3.Distance(transform.position, _nextPathPoint) < _stopDistance)
+        {
+            if (_path.corners.Length - 1 > _cornerIndex)
+            {
+                _cornerIndex++;
+                _nextPathPoint = _path.corners[_cornerIndex];
+            }
+            else
+            {
+                _hasPath = false;
+                _rechedDistination = true;
+            }
+        }
+
+        for (int i = 0; i < _path.corners.Length - 1; i++)
+            Debug.DrawLine(_path.corners[i], _path.corners[i + 1], Color.red);
+    }
+
+    private void CalculatePath(Vector3 target)
+    {
+        NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, _path);
+        
+        _hasPath = _path.corners.Length > 0;
+        _rechedDistination = false;
+
+        _cornerIndex = 1;
     }
 
     private void MoveToTarget()
     {
+        if(_nextPathPoint == null) return;
+
+        if (_rechedDistination == true)
+        {
+            _vehicle.SetTargetControl(new Vector3(0, 1, 0));
+            return;
+        }
+
         float turnControl = 0;
         float forwardThrust = 1;
 
@@ -104,7 +189,7 @@ public class AIMovement : MonoBehaviour
     private Vector3 GetReferenceMovementDirectionZX()
     { 
         var tankPos = _vehicle.transform.GetPositionZX();
-        var targetPos = _target.GetPositionZX();
+        var targetPos = _nextPathPoint.GetPositionZX();
 
         return (tankPos - tankPos).normalized;
     }
